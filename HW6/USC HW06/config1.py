bc1b777d749e8936af393e2a6262f93e933f9847
helloellenchen <33673306+helloellenchen@users.noreply.github.com>
2018-03-16 20:40:14 -0700

    Add files via upload

---
+++ HW6/USC HW06/config1.py
@@ -0,0 +1,6 @@
+# This api_key is weather, gkey is google, geokey is geocoding
+
+gkey = "AIzaSyCR-yvWX0Hh-uERy3jPN7NOkKAijm887cw"
+geokey = "AIzaSyDY0U1MulStg8VIThZ0SSawUU1oIZGXCt4"
+api_key = "25bc90a1196e6f153eece0bc0b0fc9eb"
+
